60ba99e9a244ec761cee441762cb3ef5d2b7f230
Ben Suskins <suskinsdevelopment@gmail.com>
2019-07-11 22:38:07 +0100

    Building out of logic for updating Strings and Colours ##################################################### TODO ##################################################### * Execute SOAP request and get actual return values * Migrate all code to separate class for handling this * Check out docs as to where updateScores() call should be made * Improve that list stuff thats done

--- app/src/main/java/uk/co/suskins/commutestatus/MainActivity.java
+++ app/src/main/java/uk/co/suskins/commutestatus/MainActivity.java
@@ -1,16 +1,15 @@
 package uk.co.suskins.commutestatus;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        updateStatus();
     }
 
     @Override
@@ -37,5 +37,68 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void updateStatus() {
+        //Updates the Text Views
+        //Hockley Text Views
+        TextView hockleyTime = findViewById(R.id.hockley_time);
+        TextView hockleyStatus = findViewById(R.id.hockley_status);
+
+        //Stratford Text Views
+        TextView stratfordTime = findViewById(R.id.stratford_time);
+        TextView stratfordStatus = findViewById(R.id.stratford_status);
+
+
+        //Update the Time and Status values
+        updateHockley(hockleyTime, hockleyStatus);
+        updateStratford(stratfordTime, stratfordStatus);
+
+        //Update the background colours
+        updateColor(hockleyStatus, stratfordStatus);
+    }
+
+    private void updateStratford(TextView stratfordTime, TextView stratfordStatus) {
+        updateStratfordTime(stratfordTime);
+        updateStratfordStatus(stratfordStatus);
+    }
+
+    private void updateHockley(TextView hockleyTime, TextView hockleyStatus) {
+        updateHockleyTime(hockleyTime);
+        updateHockleyStatus(hockleyStatus);
+    }
+
+    private void updateHockleyTime(TextView hockleyTime) {
+        //todo
+    }
+
+    private void updateStratfordTime(TextView stratfordTime) {
+        //todo
+    }
+
+    private void updateStratfordStatus(TextView stratfordStatus) {
+        //todo
+        stratfordStatus.setText(R.string.train_status_cancelled);
+    }
+
+    private void updateHockleyStatus(TextView hockleyStatus) {
+        //todo
+        hockleyStatus.setText(R.string.train_status_on_time);
+    }
+
+    private void updateColor(TextView hockleyStatus, TextView stratfordStatus) {
+        List<TextView> statuses = new LinkedList<>();
+        statuses.add(hockleyStatus);
+        statuses.add(stratfordStatus);
+
+        for (TextView status : statuses) {
+            if (status.getText().equals(getString(R.string.train_status_on_time))) {
+                status.setBackgroundColor(getResources().getColor(R.color.colorOnTime));
+            } else if (status.getText().equals(getString(R.string.train_status_cancelled))) {
+                status.setBackgroundColor(getResources().getColor(R.color.colorCancelled));
+            } else if (status.getText().equals(getString(R.string.train_status_delayed))) {
+                status.setBackgroundColor(getResources().getColor(R.color.colorDelayed));
+            }
+        }
     }
 }
